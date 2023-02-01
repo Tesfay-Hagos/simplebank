069822af752d34afc4e48c79b270af6c6ea21150
Tesfay Hagos <tesfay.hagos1421@gmail.com>
2023-02-01 16:15:15 +0300

    adder is done by Tesfay.H and pass the test

--- assignment_01/add/adder.go
+++ assignment_01/add/adder.go
@@ -1,5 +1,7 @@
 package add
 
+import "fmt"
+
 // Write a Go program that implements a simple calculator that can add,
 // subtract, multiply and divide two numbers. The program should take
 // the operation to be performed as a command-line argument, along with the numbers.
@@ -7,12 +9,47 @@ package add
 // 		to the calculator function
 
 func Calculator(operator string, number ...float64) float64 {
-	result := 0.0
+
+	var result float64=number[0]
+	switch operator{
+	case"add":
+		for _,i:=range number[1:]{
+			result=result+i
+
+		}
+	
+	case"subtract":
+		for _,i:=range number[1:]{
+			result=result-i
+
+		}
+		return result
+	case"multiply":
+		for _,i:=range number[1:]{
+			result=result*i
+
+		}
+	case"divide":
+		for _,i:=range number[1:]{
+			result=result/i
+
+		}
+		
+	}
 	//TODO: your code here
 	return result
 }
 
 func readFromCommandLine() {
+	var x,y float64
+	var opr string
+	fmt.Scanln(&opr)
+	fmt.Scanln(&x)
+	fmt.Scanln(&y)
+	result:=Calculator(opr,x,y)
+	fmt.Printf("the sum of and is: %f",result)
+
+
 	// should accept minimum 3 arguments
 	// call the calculator with the arguments
 
