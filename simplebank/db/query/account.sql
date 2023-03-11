1f62c328e58b2804d31a806c4b548eab562c98f1
Tesfay.H <tesfay.hagos1421@gmail.com>
2023-03-11 11:08:48 +0200

    TRAIN-146 CRUD operations generated using sqlc library for simplebank mini project

---
+++ simplebank/db/query/account.sql
@@ -0,0 +1,29 @@
+-- name: Createaccount :one
+INSERT INTO accounts (
+  owner,
+  balance,
+  currence
+) VALUES (
+  $1, $2,$3
+)RETURNING *;
+
+-- name: GetAccount :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1;
+
+-- name: ListAccounts :many
+SELECT * FROM accounts
+ORDER BY id
+LIMIT $1
+offset $2;
+
+
+-- name: UpdateAccount :one
+UPDATE accounts 
+SET balance = $2
+WHERE id=$1
+RETURNING *;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts
+WHERE id = $1;
